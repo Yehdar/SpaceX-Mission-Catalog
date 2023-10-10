c3a9292f62b6421d38976373d0a035803bbd2b5b
Yehdar <p.radhey06@gmail.com>
2023-10-10 10:50:48 -0400

    add a route handler for the root URL

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-import express from 'express'; // yarn add express
+import express from 'express';
 import { createHandler } from 'graphql-http/lib/use/express';
 import { schema } from './schema.js';
 
 // Create a express instance serving all methods on `/graphql`
-// where the GraphQL over HTTP express request handler is
 const app = express();
+const PORT = 4000;
+
+// GraphQL endpoint
 app.all('/graphql', createHandler({ schema }));
 
-app.listen({ port: 4000 });
-console.log('Listening to port 4000');
+// GET endpoint to test if the server is running
+app.get('/', (req, res) => {
+  res.send('hollup... let him cook');
+});
+
+app.listen(PORT, () => {
+  console.log('Listening to port 4000');
+});
