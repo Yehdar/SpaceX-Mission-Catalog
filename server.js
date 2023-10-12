9f11855a5f799488c6ac2a8b06f57791666408c0
Yehdar <p.radhey06@gmail.com>
2023-10-12 12:45:54 -0400

    update server.js configuration and imports

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import { createHandler } from 'graphql-http/lib/use/express';
-import { schema } from './schema.js';
+import express from "express";
+import { createHandler } from "graphql-http/lib/use/express";
+import { RootQuery } from "./schema.js";
 
 // Create a express instance serving all methods on `/graphql`
 const app = express();
 const PORT = 4000;
 
 // GraphQL endpoint
-app.all('/graphql', createHandler({ schema }));
+app.use("/graphql", createHandler({ RootQuery, graphql: true }));
 
 // GET endpoint to test if the server is running
-app.get('/', (req, res) => {
-  res.send('hollup... let him cook');
+app.get("/", (req, res) => {
+  res.send("hollup... let him cook!");
 });
 
 app.listen(PORT, () => {
-  console.log('Listening to port 4000');
+  console.log("Listening to port ${PORT}");
 });
