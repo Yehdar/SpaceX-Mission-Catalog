3d58120b2e3957abfd6e9bef699578f33672d9fa
Yehdar <p.radhey06@gmail.com>
2023-10-12 12:47:02 -0400

    feat: implement GraphQL schema and SpaceX API queries in schema.js

--- schema.js
+++ schema.js
@@ -1,10 +1,32 @@
-import { buildSchema } from 'graphql';
+import axios from "axios";
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+} from "graphql";
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+const LaunchType = GraphQLObjectType({
+  name: "Launch",
+  fields: () => ({
+    name: { type: GraphQLString },
+  }),
+});
 
-export { schema };
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get("https://api.spacexdata.com/v5/launches/latest")
+          .then((res) => res.data);
+      },
+    },
+  },
+});
 
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
